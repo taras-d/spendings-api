809c6909978a9cbec525aeaad820fcf202c0f340
taras-d <taras.datsenko@gmail.com>
2017-11-21 23:00:40 +0200

    Authentication

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,6 @@
 const ping = require('./ping/ping.service.js');
+const users = require('./users/users.service.js');
 module.exports = function (app) {
   app.configure(ping);
+  app.configure(users);
 };
